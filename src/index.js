829d5e919000d43d90065e95d3bc05c4dd9ac275
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-12-14 00:16:09 -0300

    Create Todo class

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
 import "./styles.css";
+
+class Todo {
+  constructor(title, description, dueDate, priority) {
+    this.title = title;
+    this.description = description;
+    this.dueDate = dueDate;
+    this.priority = priority;
+  }
+}
+
+const task = new Todo("Lavar la loza", "", "15-12-24", "low");
+
+console.log(task)
