d85a499e9ee5b953a174bf6311c51f80c4bda7f7
Edu Canales <131183212+educanales@users.noreply.github.com>
2024-12-16 23:29:30 -0300

    Add list property to Todo class

--- src/index.js
+++ src/index.js
@@ -1,14 +1,15 @@
 import "./styles.css";
 
 class Todo {
-  constructor(title, description, dueDate, priority) {
+  constructor(title, description, dueDate, priority, list) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
+    this.list = list;
   }
 }
 
-const task = new Todo("Lavar la loza", "", "15-12-24", "low");
+const task = new Todo("Lavar la loza", "", "15-12-24", "low", "Odin");
 
 console.log(task)
