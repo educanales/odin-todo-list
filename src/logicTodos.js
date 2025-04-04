5658253b2019016d9a023acaad02d64d1682e6d7
Edu Canales <131183212+educanales@users.noreply.github.com>
2025-04-03 21:12:53 -0300

    Separate in one module the logic that manages the todos.

---
+++ src/logicTodos.js
@@ -0,0 +1,36 @@
+import { renderTodo } from "./renderTodos";
+import { Todo } from "./object";
+
+export function addTodo(event, todos) {
+  event.preventDefault();
+
+  const newTodo = new Todo(
+    title.value,
+    description.value,
+    dueDate.value,
+    priority.value
+  );
+
+  todos.push(newTodo);
+  title.value = "";
+  description.value = "";
+  // dueDate.value = "";
+  // priority.value = "";
+  
+  console.log(todos);
+
+  renderTodo(todos);
+}
+
+export function deleteTodo(todos, id) {
+  todos.splice(id, 1);
+  renderTodo(todos);
+}
+
+export function setCompleted(todos, id) {
+  todos.map((todo) => {
+    if (todo.id === id) {
+      todo.completed = true;
+    }
+  })
+}
