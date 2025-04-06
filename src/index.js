2d662a728b43056d796241dcf4d5a5fb58c27b7f
Edu Canales <131183212+educanales@users.noreply.github.com>
2025-04-06 16:03:34 -0400

    Change import source of showProjectList

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import "./styles.css";
-import { showTodo, showProjectList } from "./dom.js";
+// import { showTodo, showProjectList } from "./dom.js";
 import { defaultProject, defaultTodos } from "./object.js";
 import { renderTodo } from "./renderTodos.js";
+import { showProjectList } from "./renderProjects.js";
 
 // showTodo(defaultTodos);
-renderTodo(defaultTodos);
 showProjectList();
+renderTodo(defaultTodos);
+
 
 // console.log(defaultProject)
