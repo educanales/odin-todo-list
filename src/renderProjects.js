3d2b6d31a976e2283b0a8b3b6a81818dafa6efc5
Edu Canales <131183212+educanales@users.noreply.github.com>
2025-04-06 23:41:38 -0400

    Refactor the code in 3 functions

--- src/renderProjects.js
+++ src/renderProjects.js
@@ -1,9 +1,25 @@
 import { myProjects, Project } from "./object";
 
-export function showProjectList() {
-  // const main = document.querySelector("main");
+export function renderProjectDialog() {
   const newProjectBtn = document.querySelector(".new-project-btn");
-  const projectList = document.querySelector(".projects-list");
+  const newProjectDialog = document.getElementById("add-project-dialog");
+  const newProjectForm = document.forms["add-project-form"];
+  const projectCancelBtn = document.getElementById("add-project-cancel");
+
+  newProjectBtn.addEventListener("click", () => {
+    newProjectDialog.showModal();
+  });
+
+  projectCancelBtn.addEventListener("click", () => newProjectDialog.close());
+
+  newProjectForm.addEventListener("submit", e => {
+    addProject(e);
+    newProjectDialog.close();
+  });
+}
+
+export function renderProjectList() {
+  const projectList = document.querySelector(".projects-list");  
   projectList.replaceChildren();
 
   myProjects.forEach((project) => {    
@@ -11,28 +27,14 @@ export function showProjectList() {
     li.textContent = project.name;
     projectList.appendChild(li);
   });
-  
-  newProjectBtn.addEventListener("click", () => {
-    const newProjectDialog = document.getElementById("add-project-dialog");
-    const newProjectForm = document.forms["add-project-form"];
-    const projectCancelBtn = document.getElementById("add-project-cancel");
-    const projectName = document.getElementById("project-name")
+}
 
-    newProjectDialog.showModal();
-
-    projectCancelBtn.addEventListener("click", () => newProjectDialog.close());
-
-    newProjectForm.addEventListener("submit", e => {
-      e.preventDefault();
-      const nameValue = projectName.value;
-      console.log(nameValue);
-      const newProject = new Project(nameValue);
-      myProjects.push(newProject);
-      // nameValue = "";
-      console.log(myProjects);
-      showProjectList();
-      newProjectDialog.close();
-    });
-    // saveBtn.addEventListener("click", () => newProjectModal.close());
-  });
+function addProject(e) {
+  e.preventDefault();
+  const nameValue = projectname.value;
+  const newProject = new Project(nameValue);
+  myProjects.push(newProject);
+  projectname.value = "";
+  // console.log(myProjects);
+  renderProjectList();
 }
