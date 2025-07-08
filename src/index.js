22bc875eba6c52c19aa9c18aa9271b54d12e81fc
Edu Canales <131183212+educanales@users.noreply.github.com>
2025-07-07 23:52:06 -0400

    Edit name of renderNewProjectDialog function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 import "./styles.css";
 import { selectedProjectId } from "./object.js";
 import { renderAddTodoDialog, renderTodos } from "./renderTodos.js";
-import { renderProjectDialog, renderProjectList } from "./renderProjects.js";
+import { renderNewProjectDialog, renderProjectList } from "./renderProjects.js";
 import { type } from "./type.js";
 
 renderProjectList();
 renderTodos();
-renderProjectDialog();
+renderNewProjectDialog();
 renderAddTodoDialog();
 
 // console.log(type(selectedProjectId));
